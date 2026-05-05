bd83d0d557f9751a4beca176dbf72877869895be
Phan Xuan Thanh <25020397@vnu.edu.vn>
2026-05-05 10:43:56 +0700

    Khoi tao Bai 5 voi JaCoCo Coverage

--- src/main/java/banksystem/BankAccount.java
+++ src/main/java/banksystem/BankAccount.java
@@ -3,8 +3,18 @@ package banksystem;
 public class BankAccount {
     private double balance;
 
-    public BankAccount(double balance) {
-        this.balance = balance;
+    public BankAccount(double initialBalance) {
+        if (initialBalance < 0) {
+            throw new IllegalArgumentException("So du khong duoc am");
+        }
+        this.balance = initialBalance;
+    }
+
+    public void deposit(double amount) {
+        if (amount <= 0) {
+            throw new IllegalArgumentException("So tien nap phai lon hon 0");
+        }
+        this.balance += amount;
     }
 
     public double getBalance() {
